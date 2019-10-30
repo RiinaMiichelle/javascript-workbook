5d0cbd31c86e00772a20a92c57ffa32e3c400f9a
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2019-10-29 20:49:29 -0500

    finished oig latin classwork

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -9,10 +9,47 @@ const rl = readline.createInterface({
 
 
 function pigLatin(word) {
+const vowels = ['a','e','i','o','u','y'];
+word = word.toLowerCase().trim();
+let storeIndex = 0;
 
-  // Your code here
+//if (word[0] === 'a' || 'e' || 'i' || 'o' || 'u') {
+  //  return word + 'yay';
+ // }
+//}
 
+
+//const firstLetter = word[0];
+
+
+
+for (let i=0; i<word.length; i++) {
+  //if (vowels.includes(firstLetter)) {
+  //   return word + 'yay';
+  // } else {return word + 'ay';}
+  for (let j=0; j<vowels.length; j++) {
+    if (word[i] === vowels[j]){
+      storeIndex = i;
+      const firstString = word.slice(0,storeIndex);
+      const secondString = word.slice(storeIndex);
+    if (storeIndex === 0){
+      return secondString + firstString + "yay";
+    }
+    return secondString + firstString + "ay";
+      }
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 function getPrompt() {
