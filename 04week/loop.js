82a8634c8b09578c92f5d07f52c7b522dccd1e10
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2019-11-14 20:51:24 -0600

    started loop hw

---
+++ 04week/loop.js
@@ -0,0 +1,31 @@
+//Use a do...while loop to console.log the numbers from 1 to 1000.
+var result = "";
+var i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 1000);
+
+console.log(result);
+// expected result: "12345"
+
+
+//Create an object (an array with keys and values) called person with the following data:
+// firstName: "Jane"
+// lastName: "Doe"
+// birthDate: "Jan 5, 1925"
+// gender: "female"
+
+//Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
+
+//Create an arrayOfPersons that contains mulitiple objects. You can simply copy/paste the person object you made above multiple times. Feel free to change the values to reflect multiple people you might have in your database.
+
+
+//Use .map() to map over the arrayOfPersons and console.log() their information.
+
+
+//Use .filter() to filter the persons array and console.log only males in the array.
+
+
+//Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
