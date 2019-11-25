f9ede5192600cbabb18f497d530106b338faf0c1
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2019-11-24 22:15:21 -0600

    finished checkers

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -8,15 +8,50 @@ const rl = readline.createInterface({
 });
 
 
-function Checker() {
+class Checker {
   // Your code here
-}
+  constructor(color, id) {
+    this.id = id;
+    if (color === 'white') {
+      this.symbol = String.fromCharCode(0x125CB);
+    } else if (color === 'black') {
+        this.symbol = String.fromCharCode(0x125CF);
+      }
+    }
+  }
+
 
 class Board {
   constructor() {
-    this.grid = []
+    this.grid = [];
+    this.checkers = [];
   }
   // method that creates an 8x8 array, filled with null values
+  createCheckers() {
+    let whitePositions = 
+    [[0, 1], [0, 3], [0, 5], [0, 7],
+    [1, 0], [1, 2], [1, 4], [1, 6],
+    [2, 1], [2, 3], [2, 5], [2, 7]];
+
+    let blackPositions = 
+    [[5, 0], [5, 2], [5, 4], [5, 6],
+    [6, 1], [6, 3], [6, 5], [6, 7],
+    [7, 0], [7, 2], [7, 4], [7, 6]];
+
+    // We made the initializeCheckers function to eliminate code duplication of placing checkers when the game starts
+    this.initializeCheckers(whitePositions, 'white');
+    this.initializeCheckers(blackPositions, 'black');
+  }
+
+  initializeCheckers(checkersArray, color) {
+    for (let i=0; i<checkersArray.length; i++) {
+      let checker = new Checker(color, `${color}-${i}`);
+      this.checkers.push(checker);
+      const checkerCoordinates = checkersArray[i];
+      this.grid[checkerCoordinates[0]][checkerCoordinates[1]] = checker;
+    }
+  }
+
   createGrid() {
     // loop to create the 8 rows
     for (let row = 0; row < 8; row++) {
@@ -27,6 +62,7 @@ class Board {
       }
     }
   }
+
   viewGrid() {
     // add our column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
@@ -52,16 +88,88 @@ class Board {
     console.log(string);
   }
 
-  // Your code here
+  selectChecker(row, column) {
+    return this.grid[row][column];
+  }
+
+  // coordinatePair - the coordinate in the grid to kill the check ex) [0, 1]
+  killChecker(coordinatePair) {
+    const row = coordinatePair[0];
+    const column = coordinatePair[1];
+    // Go get the checker in the grid at the coordinates
+    const checker = this.selectChecker(row, column);
+
+    // Remove the checker from the checkers array (using splice)
+    //     const indexOfCheckerInArray = this.checkers.indexOf((checkerInArray) => checkerInArray.id === checker.id);
+    //     const indexOfCheckerInArray = this.checkers.indexOf((checkerInArray) => { return checkerInArray.id === checker.id });
+    const indexOfCheckerInArray = this.checkers.indexOf(function (checkerInArray) { return checkerInArray.id === checker.id });
+    // Checker is dead. Remove from the board (set to null)
+    this.checkers.splice(indexOfCheckerInArray, 1) 
+    this.grid[row][column] = null;
+  }
 }
 
 class Game {
   constructor() {
-    this.board = new Board;
+    this.board = new Board();
   }
   start() {
     this.board.createGrid();
+    this.board.createCheckers();
   }
+
+  // start is the coordinates for the start in the grid ex) '50'
+  // end is the coordinates for where we're moving the checker in the grid ex) '20'
+  moveChecker(startCoordinateString, endCoordinateString) {
+    const start = stringCoordinateToArrayCoordinate(startCoordinateString);
+    const end = stringCoordinateToArrayCoordinate(endCoordinateString);
+
+    const startRow = start[0];
+    const startColumn = start[1];
+
+    const endRow = end[0];
+    const endColumn = end[1];
+
+    const checker = this.board.selectChecker(startRow, startColumn);
+
+    // if the checker killed another checker (moved more than one row) then call this.board.killChecker for the killed checker
+    const didCheckGetKilled = Math.abs(endRow - startRow) > 1;
+
+    if (didCheckGetKilled) {
+      // determine the row of the killed checker
+      let killedCheckerRow;
+      if (startRow > endRow) {
+        // if start row is greater than end row then we know the checker moved up in the board (towards the top of the board)
+        killedCheckerRow = startRow - 1;
+      } else {
+        // checker was moved down towards the end of the board
+        killedCheckerRow = startRow + 1;
+      }
+  
+      // determine the column of the killed checker
+      let killedCheckerColumn = startColumn; // the column might now have changed
+      if (startColumn != endColumn) {
+        // the column changed ... see if the checker moved left or right on the board
+        if (startColumn > endColumn) {
+          // the checker moved left diagonaly
+          killedCheckerColumn = startColumn - 1;
+        } else {
+          // the checker moved right diagonaly
+          killedCheckerColumn = startColumn + 1;
+        }
+      }
+
+      const killedCheckerLocation = [killedCheckerRow, killedCheckerColumn];
+      this.board.killChecker(killedCheckerLocation);
+    }
+
+    this.board.grid[endRow][endColumn] = checker;
+  }
+}
+
+function stringCoordinateToArrayCoordinate(stringCoordinate) {
+  const strSplitByCharacter = stringCoordinate.split('');
+  return strSplitByCharacter.map(function (intString) { return parseInt(intString); });
 }
 
 function getPrompt() {
---
+++ 05week/checkers.md
@@ -0,0 +1,21 @@
+Board 
+8x8 = 64 (this grid)
+this.checkers(white/black pieces in play)
+createGrid, viewGrid
+create checkers (put all the checkers on the board)
+select checker(given position)
+kill checker (this.checkers / givin position)
+
+
+Symbol
+
+
+
+
+Game Class
+board, instance of board class,
+ board start
+
+ moveChecker - takes in coordinates (string) ('50', '42')
+
+ 
